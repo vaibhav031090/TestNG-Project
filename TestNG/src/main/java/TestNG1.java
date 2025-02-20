b7ffae773fb3981aa61d3657522ce26929d9df32
Dell <Dell@DESKTOP-SKOGRIB>
2025-02-19 23:20:27 -0500

    Test First commit

---
+++ TestNG/src/main/java/TestNG1.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestNG1 {
+	
+	
+	
+	@Test
+	public void test()
+	{
+		
+		WebDriverManager.chromedriver().setup();		
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com/");
+	}
+
+}
